9c497ac2aa4b36330ce1e77280b04c708a06ddec
Aman Rawat <amanrawat0919@gmail.com>
2021-04-09 20:40:35 +0530

    deploy my-music

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import Player from "./components/Player";
+import songs from "./songs";
+
+function App() {
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+
+  return (
+    <div className="App">
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/songs.js
@@ -0,0 +1,41 @@
+
+const songs = [
+  {
+    title: "Mockingbird",
+    artist: "Eminem",
+    img_src: "./images/song-1.jfif",
+    src: "./music/Mockingbird.mp3"
+  },
+  {
+    title: "Forever",
+    artist: "Lewis Capaldi",
+    img_src: "./images/song-2.jfif",
+    src: "./music/Forever.mp3"
+  },
+  {
+    title: "Old Town Road",
+    artist: " Billy Ray Cyrus and Lil Nas X",
+    img_src: "./images/song-3.jfif",
+    src: "./music/Old-town-road.mp3"
+  },
+  {
+    title: "Please Don't Go",
+    artist: "Joel Adams",
+    img_src: "./images/song-4.jfif",
+    src: "./music/please-don't-go.mp3"
+  },
+  {
+    title: "Dance Monkey",
+    artist: "Tones and I",
+    img_src: "./images/song-5.jfif",
+    src: "./music/Dance-monkey.mp3"
+  },
+  {
+    title: "Way Back Home",
+    artist: "Shaun",
+    img_src: "./images/song-6.jfif",
+    src: "./music/way-back-home.mp3"
+  }
+   
+];
+export default songs;
